7f23aece389871f216b4095297c8a386ab89bd4e
REiman CAncino <jrbcancino27@gmail.com>
2025-03-23 00:50:22 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SignatureController;
 use Livewire\Volt\Volt;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Home Route (Volt component for search)
+Volt::route('/', 'search')->name('home');
+Volt::route('/search', 'search')->name('search');
 
-Volt::Route('search', 'search')->name('search');
+// Signature Pad Routes
+Route::get('/tutorial', [SignatureController::class, 'index'])->name('signpad.index'); // Change this to /tutorial
+// Route::post('/save', [SignatureController::class, 'save'])->name('signpad.save');
+Route::post('/save', [SignatureController::class, 'saveSignature'])->name('signpad.save');
+// Route::delete('/delete/{id}', [SignatureController::class, 'delete'])->name('signpad.delete');
+Route::delete('/signpad/{id}', [SignatureController::class, 'delete'])->name('signpad.delete');
+
+
+// ----- old syntax from main -----
+// use Illuminate\Support\Facades\Route;
+// use Livewire\Volt\Volt;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Volt::Route('search', 'search')->name('search');
+
+
+
+# kalat ng routes hahahahaha
