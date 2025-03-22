94c521abcce44ed866a8c1ad2f2c1fa1d5c7fe1d
REiman CAncino <jrbcancino27@gmail.com>
2025-03-23 01:00:33 +0800

    Create 2025_03_21_071640_create_signatures_table.php

---
+++ database/migrations/2025_03_21_071640_create_signatures_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('signatures', function (Blueprint $table) {
+            $table->id();
+            $table->longText('sign_image');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('signatures');
+    }
+};
